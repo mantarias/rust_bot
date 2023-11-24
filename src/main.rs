cef3093c4d967f0b0e3df01d0edeccd5b61779de
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2023-11-24 13:31:04 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 //! This file is the main entry point for the bot. It sets up the bot and starts it.
-//! Will printout a message if updated by a user.
+//! Will print out a message if updated by a user.
 use dotenv::dotenv;
 use std::env;
+use std::fmt::format;
 use songbird::SerenityInit;
 
 use serenity::model::id::{ChannelId, MessageId};
@@ -14,6 +15,7 @@ use std::str::FromStr;
 mod commands;
 mod web;
 
+use tokio_postgres::{NoTls, Error};
 use commands::GENERAL_GROUP;
 
 struct Handler;
@@ -23,11 +25,23 @@ impl EventHandler for Handler {}
 
 #[tokio::main]
 async fn main() {
-    // Start the web server in a separate async task
-    tokio::spawn(async {
-        web::run_server().await;
+    // Connect to the database.
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost port=5432 dbname=rustbot user=postgres", NoTls).await.unwrap();
+
+    // The connection object performs the actual communication with the database,
+    // so spawn it off to run on its own.
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
     });
 
+
+    // Start the web server in a separate async task
+    tokio::spawn(async {
+        web::run_server(client).await;
+    });
 
     // Configure the client with the bot's prefix and commands
     let framework = StandardFramework::new()
@@ -35,14 +49,13 @@ async fn main() {
         .configure(|c| c.prefix("-")); // set the bot's prefix to "~"
 
     // Login with a bot token from the environment
-
     dotenv().ok();
     let token = env::var("DISCORD_BOT_TOKEN").expect("Expected a token in the environment");
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .register_songbird()
-
         .framework(framework) // framework is now used here after all configurations
         .await
         .expect("Error creating client");
@@ -70,6 +83,7 @@ async fn main() {
             }
         }
     }
+
     // Start listening for events by starting a single shard
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
--- src/web/web_main.rs
+++ src/web/web_main.rs
@@ -1,10 +1,15 @@
-use axum::{body::Body, extract::{Query, Path}, http::{Response, StatusCode}, routing::get, routing::post, Router, Json};
-use serde_derive::{Deserialize, Serialize};
-use std::net::SocketAddr;
-use std::path::PathBuf;
-use tokio::io::AsyncReadExt;
+use axum::{
+    body::Body, extract::{Extension, Json, Path, Query}, http::{Response, StatusCode},
+    routing::get, routing::post, Router,
+};
 use axum::response::Html;
+use serde_derive::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::net::SocketAddr;
 use tokio::fs;
+use tokio::io::AsyncReadExt;
+use tokio_postgres::Client;
+use std::sync::Arc;
 
 
 #[derive(Deserialize)]
@@ -18,13 +23,24 @@ struct ResponseData {
     message: String,
 }
 
+
 #[derive(Deserialize)]
 struct QueryParams {
     page: Option<String>,
 }
 
-pub async fn run_server() {
+pub async fn run_server(client: Client) {
+    // Create table if it doesn't exist
+    client
+        .execute(
+            "CREATE TABLE IF NOT EXISTS commands (command_name text, command_response text)",
+            &[],
+        )
+        .await
+        .unwrap();
+
     let app = Router::new()
+        .layer(Extension(Arc::new(client)))
         .route("/", get(page_handler))
         .route("/post", post(post_handler))
         .route("/static/*path", get(static_handler));
@@ -59,12 +75,31 @@ async fn serve_html_file(path: &str) -> Result<Html<String>, (StatusCode, String
     }
 }
 
-async fn post_handler(Json(data): Json<PostData>) -> Json<ResponseData> {
+async fn post_handler(Extension(client): Extension<Arc<Client>>, Json(data): Json<PostData>) -> Json<ResponseData> {
     println!("Received field1: {} and {}", data.field1, data.field2);
 
     let response = ResponseData {
         message: format!("Received field1: {} and {}", data.field1, data.field2),
     };
+
+    client
+        .execute(
+            "INSERT INTO hello (column_name) VALUES ($1)",
+            &[&"hello world"],
+        )
+        .await
+        .unwrap();
+
+    let rows = client
+        .query("SELECT (column_name) FROM hello", &[])
+        .await.expect("Failed to run query");
+
+// You can use this to print the value of 'column_name' in the first row
+    for row in rows {
+        let column_value: String = row.get("column_name");
+        println!("{}", column_value);
+    }
+
 
     Json(response)
 }
