0186478e1eb09faa7be674356d049dd54ea0720f
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2023-11-25 19:42:42 +0100

    Update tailwind.config.js

--- src/web/www/tailwind.config.js
+++ src/web/www/tailwind.config.js
@@ -10,30 +10,7 @@ module.exports = {
 
     daisyui: {
         themes: [
-            {
-                light: {
-                    "primary": "#00dbe6",
-                    "secondary": "#00a500",
-                    "accent": "#efb500",
-                    "neutral": "#141414",
-                    "base-100": "#fef9ff",
-                    "info": "#00abd6",
-                    "success": "#00a85f",
-                    "warning": "#ffaf00",
-                    "error": "#d00014",
-                },
-                dark: {
-                    "primary": "#f59e0b",
-                    "secondary": "#facc15",
-                    "accent": "#4ade80",
-                    "neutral": "#3f2d29",
-                    "base-100": "#342c23",
-                    "info": "#7dd3fc",
-                    "success": "#a3e635",
-                    "warning": "#fc6000",
-                    "error": "#f43f5e",
-                },
-            },
+            "dark"
         ],
     },
 };
